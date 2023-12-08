6f44b6dfbda3de39dcb10d58a83f68de974bafd5
mervekabakci-github <116270494+mervekabakci-github@users.noreply.github.com>
2023-12-08 17:15:24 +0300

    genel layout yapısı kuruldu

---
+++ resources/views/include/shared/header.blade.php
@@ -0,0 +1,5 @@
+<header class="pt-4 position-absolute w-100" data-aos="fade-down"  data-aos-duration="1000" data-aos-easing="ease">
+    <div class="container-xxxl px-xxxl-0 pe-0 pt-2 ps-5 ps-xxxl-0">
+        @include('include.navbar')
+    </div>
+</header>
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/assets/front/css/app.scss', 'esources/assets/front/js/app.js'],
             refresh: true,
         }),
     ],
