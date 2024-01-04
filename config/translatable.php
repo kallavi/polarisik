aa67f97fff7a6896b05ee637f6d571c241f81ab0
Bahadır Nişancı <bhdrnsnci@gmail.com>
2024-01-04 18:14:57 +0300

    pl-46

--- config/translatable.php
+++ config/translatable.php
@@ -80,7 +80,7 @@ return [
     | is used first. So "es" will be checked before "es_MX".
     |
     */
-    'fallback_locale' => 'tr',
+    'fallback_locale' => null,
 
     /*
     |--------------------------------------------------------------------------
--- resources/views/admin/pages/login/index.blade.php
+++ resources/views/admin/pages/login/index.blade.php
@@ -13,11 +13,8 @@
     <div class="d-flex flex-column flex-column-fluid flex-lg-row align-items-center">
         <div class="d-flex flex-center w-lg-50 pt-15 pt-lg-0 px-10">
             <div class="d-flex flex-center flex-lg-start flex-column">
-                <a href="{{ route('backoffice.index') }}" class="mb-7">
-                    {{-- @php
-                    dd($data['setting']['dark_logo']);
-                @endphp --}}
-                    <img alt="Logo" src="{{ asset($data['setting']['dark_logo']) }}" />
+                <a class="mb-7">
+                    <img alt="Logo" src="/uploads/setting/657757d2cec9a.svg" />
                 </a>
             </div>
         </div>
@@ -28,15 +25,9 @@
                 <!--begin::Wrapper-->
                 <div class="d-flex flex-center flex-column flex-column-fluid px-lg-10 py-15 py-lg-20">
                     <!--begin::Form-->
-                    {{-- <div class="d-flex flex-center flex-lg-start flex-column mb-11">
-                    <img alt="Logo" src="{{asset('front/assets/images/static/logos/logo.svg')}}" />
-                </div> --}}
 
                     <x-admin.form :class="'form w-100'" :novalidate="'novalidate'" :id="'kt_sign_in_form'" :action="'/authAdmin'" :method="'POST'">
 
-                        {{-- <x-slot:dataKtUrl>
-                        {{route('home')}}
-                    </x-slot:dataKtUrl> --}}
                         <x-slot:form>
                             <x-admin.tags-wrapper :class="'text-center mb-11'">
                                 <x-slot:tagsWrapper>
@@ -49,31 +40,6 @@
 
                             <button class="btn text-white btn-danger w-100">Giriş Yap</button>
 
-                            {{-- <x-admin.button
-                            :buttonParentClass="'d-grid'"
-                            :class="'btn text-white btn-active-danger'"
-                            :id="'kt_sign_in_submit'"
-                            :color="'indigo'"
-                            :type="'submit'"
-                        >
-                            <x-slot:title>
-                                <x-admin.form-label
-                                    :title="'Giriş Yap'"
-                                    :titleTag="'span'"
-                                    :class="'indicator-label'"
-                                />
-                            </x-slot:title>
-                            <x-slot:buttonContain>
-                                <x-admin.tags-wrapper :tags="'span'" :class="'indicator-progress'" :title="'Lütfen bekleyiniz...'">
-                                    <x-slot:tagsWrapper>
-                                        <x-admin.form-label
-                                        :title="''"
-                                        :titleTag="'span'"
-                                        :class="'spinner-border spinner-border-sm align-middle ms-2'" />
-                                    </x-slot:tagsWrapper>
-                                </x-admin.tags-wrapper>
-                            </x-slot:buttonContain>
-                        </x-admin.button> --}}
                         </x-slot:form>
 
                         @csrf
@@ -92,7 +58,6 @@
 @section('script')
     @if ($errors->all() != null)
         <script>
-             
             Swal.fire({
                 title: "Hatalı Giriş!",
                 text: "Girilen e-mail veya şifre hatalıdır. Lütfen tekrar deneyiniz.",
@@ -103,8 +68,6 @@
                     confirmButton: "btn btn-primary"
                 }
             });
-
-          
         </script>
     @endif
 @endsection
